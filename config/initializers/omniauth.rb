f2c0bb7f659bda08889ac45b71f50537863fa289
Shailesh <hegde.shailesh@gmail.com>
2011-09-22 00:04:41 -0400

    added FB omniauth secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'RatoMKzVUMq9mFeR4XEhFw', 'QKvFjj4Th111zLBxK4zS5KxvdThuewQRBEmbFe4Mng'
-  provider :facebook, 'APP_ID', 'APP_SECRET'
+  provider :facebook, '259787160728620', '0cb284ff40c86eb83096f51300da432b'
   provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
 end

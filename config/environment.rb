adf481cc89e984ab3c487a02d3683be44cf56a59
Shailesh <hegde.shailesh@gmail.com>
2011-09-27 01:05:27 -0400

    installed active merchant for testing payments

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+require 'active_merchant'
 # Initialize the rails application
 Hobster::Application.initialize!
+
+

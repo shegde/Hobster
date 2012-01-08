260d41285b5673e9f059612ec1bee3bf7d09e835
Shailesh <hegde.shailesh@gmail.com>
2012-01-08 15:50:41 -0500

    updated login actions

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,6 +1,6 @@
 class Registration < ActiveRecord::Base
   
-  belongs_to :user, :foreign_key => "user_id"
-  belongs_to :post, :foreign_key => "post_id"
+  belongs_to :user 
+  belongs_to :post
   
 end
